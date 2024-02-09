9ff0efb38c7586ea95989c8805635b31ee04e7fc
Andrea Furtunato <124641425+andreafdev@users.noreply.github.com>
2024-02-09 18:27:31 -0300

    projeto finalizado

--- index.js
+++ index.js
@@ -96,37 +96,37 @@ const perguntas = [
   
   const corretas = new Set()
   const totalDePerguntas = perguntas.length
-  const mostrarTotal =  document.querySelector('#acertos span')
+  const mostrarTotal = document.querySelector('#acertos span')
   mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
   
-  //laço de repetição
-  for (const item of perguntas){
-  const quizItem = template.content.cloneNode(true)
-  quizItem.querySelector('h3').textContent = item.pergunta
+  // loop ou laço de repetição
+  for (const item of perguntas) {
+    const quizItem = template.content.cloneNode(true)
+    quizItem.querySelector('h3').textContent = item.pergunta
   
-  for(let resposta of item.respostas) {
-  const dt = quizItem.querySelector('dl dt').cloneNode(true)
-  dt.querySelector('span').textContent = resposta
-  dt.querySelector('input').setAttribute('name','pergunta-' + perguntas.indexOf(item))
-  dt.querySelector('input').value = item.respostas.indexOf(resposta)
-  dt.querySelector('input').onchange = (event) => {
-    const estaCorreta = event.target.value == item.correta
+    for (let resposta of item.respostas) {
+      const dt = quizItem.querySelector('dl dt').cloneNode(true)
+      dt.querySelector('span').textContent = resposta
+      dt.querySelector('input').setAttribute('name', 'pergunta-' + perguntas.indexOf(item))
+      dt.querySelector('input').value = item.respostas.indexOf(resposta)
+      dt.querySelector('input').onchange = (event) => {
+        const estaCorreta = event.target.value == item.correta
   
-    corretas.delete(item)
-    if(estaCorreta) {
-      alert('acertou')
-        corretas.add(item)
+        corretas.delete(item)
+        if (estaCorreta) {
+          corretas.add(item)
+        }
+  
+        mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
+      }
+      quizItem.querySelector('dl').appendChild(dt)
     }
-    mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
-  }
   
-  quizItem.querySelector('dl').appendChild(dt)
   
-  }
+    quizItem.querySelector('dl dt').remove()
   
-  quizItem.querySelector('dl dt').remove()
   
-  //coloca a pergunta na tela
-  quiz.appendChild(quizItem)
+    // coloca a pergunta na tela
+    quiz.appendChild(quizItem)
   }
   
